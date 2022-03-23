fbe18d83b8a50f246cbac7e776e5591d90adca10
prathamesh-khadakban <prathamesh.khadakban@gmail.com>
2022-03-23 10:56:23 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 //   - Javascript is synchonus,single-threaded language.
 
+// closures
 function a() {
   let x = 10;
   return function b() {
@@ -12,3 +13,21 @@ function a() {
 }
 const z = a();
 console.log(z());
+
+//call - example
+// function borrowing
+// access the function adn properties of another object
+
+let data = {
+  name: 'abc',
+  age: 21,
+};
+
+let greet = function (place) {
+  console.log(`this is ${this.name} and he live in ${place}`);
+};
+
+greet.call(data, 'badlapur');
+//data.call(greet);
+
+
