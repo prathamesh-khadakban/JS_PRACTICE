6e721ee060d30dba47b1ed3faee8eef7fbad280c
prathamesh-khadakban <prathamesh.khadakban@gmail.com>
2022-03-23 09:55:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 //   - thread Execution
 
 //   - Javascript is synchonus,single-threaded language.
+
+function a() {
+  let x = 10;
+  return function b() {
+    console.log(x);
+  };
+}
+const z = a();
+console.log(z());
